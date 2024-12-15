67aeecc572d3a4e22b06478d1f3c8bdb8b2983a7
jeff-stienstra <jeff.stienstra@envoyb2b.com>
2024-12-14 20:35:35 -0500

    update createUser and styling

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -2,18 +2,19 @@ import {useEffect, useState} from 'react'
 import './Home.css'
 
 interface Customer {
-  id: number
   name: string
   company: string
 }
 interface User {
-  id: number
   name: string
+  age: string
 }
 
 function App() {
   const [customers, setCustomers] = useState<Customer[]>([]);
+  const [showCreateUserModal, setShowCreateUserModal] = useState(false);
   const [users, setUsers] = useState<User[]>([]);
+  const [newUser, setNewUser] = useState<User>({name: '', age: ''});
 
   useEffect(() => {
     fetch('http://localhost:5000/api/users')
@@ -36,17 +37,30 @@ function App() {
     );
   }, []);
 
-function createUser() {
-  fetch('http://localhost:5000/api/users', {
-    method: 'POST',
-    // headers: {
-    //   'Content-Type': 'application/json'
-    // },
-    body: JSON.stringify({
-      name: 'New User'
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNewUser({ ...newUser, [name]: value });
+  };
+
+  function createUser(e: React.FormEvent) {
+    e.preventDefault();
+    fetch('http://localhost:5000/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
     })
-  })
-}
+    .then(response => response.json())
+    .then(user => {
+      console.log('User created:', user);
+      setUsers([...users, user]);
+      setShowCreateUserModal(false);
+    })
+    .catch(error => {
+      console.error('Error creating user:', error);
+    });
+  };
 
 function deleteUser(user: User) {
   console.log('Deleting user:', user)
@@ -55,6 +69,9 @@ function deleteUser(user: User) {
   })
 }
 
+function handleToggleCreateUserModal() {
+  setShowCreateUserModal(!showCreateUserModal)
+}
 // function createCustomer() {
 //   fetch('http://localhost:5000/api/customers', {
 //     method: 'POST',
@@ -67,20 +84,55 @@ function deleteUser(user: User) {
       <div className="card">
         <h3>Users</h3>
         {users?.map((user) => (
-          <div key={user.name}>
+          <div className='data-block' key={user.name}>
             <p key={user.name}>{user.name}</p>
-            <button onClick={() => deleteUser(user)}>-User</button>
+            <button className='button' onClick={() => deleteUser(user)}>
+                delete
+            </button>
           </div>
         ))}
       </div>
-        <button onClick={createUser}>+User</button>
+        <button className='button' onClick={handleToggleCreateUserModal}>+User</button>
       <div className="card">
         <h3>Customers</h3>
         {customers?.map((customer) => (
           <p key={customer.name}>{`${customer.name}: ${customer.company}`}</p>
         ))}
       </div>
-        {/* <button onClick={createCustomer}>+Customer</button> */}
+
+      {/* MODALS */}
+      {/* create user */}
+      {showCreateUserModal && (
+        <div className="modal-background">
+          <form onSubmit={createUser} className="modal-container">
+            <h3 className='modal-title'>Create User</h3>
+            <div className="input-container">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name='name'
+                onChange={handleInputChange}
+                placeholder="Name"
+                required
+              />
+              <label htmlFor="age">Age</label>
+              <input
+                type="text"
+                id="age"
+                name='age'
+                onChange={handleInputChange}
+                placeholder="Age"
+                required
+              />
+            </div>
+            <div className="button-container">
+              <button type='submit' className='button btn-submit'>Create</button>
+              <button type='button' className='button btn-cancel' onClick={handleToggleCreateUserModal}>Cancel</button>
+            </div>
+          </form>
+        </div>
+      )}
     </>
   )
 }

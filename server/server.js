5cfc542c44cd3493450761173829ff25c06ff54d
jeff-stienstra <jeff.stienstra@envoyb2b.com>
2024-12-19 15:51:59 -0500

    rename server from ‘test’ to ‘example-server’

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express();
 const PORT = 5000;
 
-mongoose.connect('mongodb://localhost:27017/test')
+mongoose.connect('mongodb://localhost:27017/example-server')
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));;
 
